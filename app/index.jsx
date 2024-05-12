a84bd8fa570d1015f5f09501c9040114fd8558cb
Aayush Jain <aayush2@stanford.edu>
2024-05-12 16:49:51 -0700

    auth
    auth

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
+// Updated Firebase imports for modular API
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from './components/Landing.js';
+
+import Register from './components/auth/Register'; // Make sure this path is correct
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQoKtYGhwUP7Fne8Mi3TgYebKFXLVVUso",
+  authDomain: "trabitapp.firebaseapp.com",
+  projectId: "trabitapp",
+  storageBucket: "trabitapp.appspot.com",
+  messagingSenderId: "355540549767",
+  appId: "1:355540549767:web:c39ac1db1d9d87a4303822",
+  measurementId: "G-RPT6V0G1BV"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Phone Auth</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
