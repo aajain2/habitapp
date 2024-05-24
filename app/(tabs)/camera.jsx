061a9fea7a1c7cb4a4056c69fac6043937a662b8
williamzhu17 <williammzhu@gmail.com>
2024-05-24 15:59:40 -0700

    camera now has functionality

--- app/(tabs)/camera.jsx
+++ app/(tabs)/camera.jsx
@@ -1,20 +1,25 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
 import TrabitHeader from '../../components/TrabitHeader'
 import BackButton from '../../components/buttons/BackButton'
 import { StatusBar } from 'expo-status-bar'
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import { router } from 'expo-router'
+import icons from '../../constants/icons'
 
 const Camera = () => {
-  const [facing, setFacing] = useState('front');
-  const [permission, requestPermission] = useCameraPermissions();
+  const [facing, setFacing] = useState("front")
+  const [flash, setFlash] = useState(false)
+  const [permission, requestPermission] = useCameraPermissions()
+  const [cameraReady, setCameraReady] = useState(false)
 
-  // useEffect(() => {
-  //   if (!permission.granted) {
-  //     requestPermission()
-  //   }
-  // }, [permission])
+  let camera
+
+  const takePicture = async () => {
+    if (camera && cameraReady) {
+      camera.takePictureAsync({ onPictureSaved: (data) => console.log(data) });
+    }
+  }
 
   return (
     <SafeAreaView>
@@ -29,8 +34,45 @@ const Camera = () => {
         <View className="flex items-center justify-center h-full">
           <View className="w-[90vw] h-[60vh] rounded-2xl bg-light-gray">
             {permission && 
-              <CameraView facing={facing} className="w-full h-full rounded-2xl overflow-hidden"></CameraView>
+              <CameraView 
+                facing={facing} 
+                className="w-full h-full rounded-2xl overflow-hidden" 
+                flash={flash ? "on" : "off"}
+                onCameraReady={() => setCameraReady(true)}
+                ref={(ref) => { camera = ref }}
+              />
             }
+          </View>
+
+          <View className="flex-row justify-center items-center w-[90vw] gap-x-8 mt-8">
+            <TouchableOpacity onPress={() => setFlash(!flash)}>
+              <Image 
+                className="w-14 h-14"
+                source={flash ? icons.flashOn : icons.flashOff}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+              className="w-24 h-24 border-[10px] rounded-full"
+              onPress={() => takePicture()}
+            />
+
+            <TouchableOpacity 
+              onPress={() => {
+                if (facing === "front") {
+                  setFacing("back")
+                } else {
+                  setFacing("front")
+                }
+              }}
+            >
+              <Image 
+                className="w-14 h-14"
+                source={icons.cameraFlip}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
